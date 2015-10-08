95451afa5af19e4c8033744178f094003a21726e
Richard <richard.lupton@gmail.com>
2015-10-08 13:36:07 +0100

    Rewrote laboratory.hs a little to make it more abstract. It would be nice to write it using monads to abstract away some of the details of SDL, but reactimate is currently to restrictive. This may change in future however. This file could perhaps be further restructured to form a small library. There are many options present here. Currently the SDL surfaces aren't fed through the Yampa code. This seems to make sense, but for a bigger program, stronger structures would be nice.

--- src/laboratory.hs
+++ src/laboratory.hs
@@ -2,22 +2,35 @@ import Graphics.UI.SDL as SDL
 import FRP.Yampa as Y
 import YampaUtils
 
---type Position = (Double, Double)
+-- It would be nice to structure this program using monads,
+-- but the type of reactimate is presently too restrictive.
 
---type Interface = Interface { screen :: Surface }
+type Position = (Double, Double)
+data Object = Object Position
 
---data Object = Object Sprite (Double, Double)
---data Sprite = Sprite String Surface
+data ProcessedSample = ProcessedSample [Object] Bool
 
-output :: Surface -> Surface -> ((Int, Int), Bool) -> IO Bool
-output object surface ((x,y), exit) = do
-    fillRect surface Nothing (Pixel $ 0)
-    blitSurface object Nothing surface (Just $ Rect x y 0 0)
+clearSurface :: Surface ->  IO Bool
+clearSurface surface = fillRect surface Nothing (Pixel $ 0)
+
+renderTo :: Surface ->  Object -> Surface -> IO Bool
+renderTo surface (Object (x,y)) image =
+  blitSurface image Nothing surface (Just $ Rect (round x) (round y) 0 0)
+
+output :: Surface -> [Surface] -> ProcessedSample -> IO Bool
+output surface sfs pss = do
+    clearSurface surface
+    let (ProcessedSample obs exit) = pss
+    sequence $ zipWith (renderTo surface) obs sfs
     SDL.flip surface
     return exit
 
-sigFun :: SF Bool ((Int, Int), Bool)
-sigFun = ((time >>> arr (\x -> floor $ 300 - exp (5-x))) &&& (time >>> arr (\x -> (+100).ceiling.(*20) $ sin (5*x)))) &&& identity
+sigFun :: SF Bool ProcessedSample
+sigFun = ((time >>>
+           (arr (\x -> 300 - exp (5-x)) &&& arr (\y -> (+100).(*20) $ sin (5*y)))
+           >>> arr (Object) >>> arr (\x -> [x]))
+           &&& identity)
+          >>> arr (uncurry(ProcessedSample))
 
 -- Poll Events gets all the pending events in one go for processing
 pollEvents :: IO [SDL.Event]
@@ -38,8 +51,8 @@ main = do
     SDL.init [InitEverything]
 
     screen <- setVideoMode 640 480 32 [SWSurface] -- We want to thread this through the application
-    hello  <- loadBMP "manSprite.bmp"
+    person  <- loadBMP "manSprite.bmp"
     
-    yampaMain (return False) input (output hello screen) sigFun
+    yampaMain (return False) input (output screen [person]) sigFun
 
     SDL.quit
